3cf02eb448a326b586c9e560f061c8cfcc7e9f32
abror <163519023+Abror2707@users.noreply.github.com>
2024-08-02 17:28:55 +0500

    Add files via upload

---
+++ UI/login.js
@@ -0,0 +1,13 @@
+
+
+let login = document.querySelector('.login');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+login.addEventListener('click', () => {
+  if (username.value === 'admin' && password.value === '1234') {
+    window.location.href = 'index2.html';
+  } else {
+    alert('login yoki parol notogri');
+  }
+});
